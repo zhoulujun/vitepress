b462867fd8c6a233f3b5cdab027c4be82b9c1304
Kia King Ishii <kia.king.08@gmail.com>
2022-06-07 01:25:52 +0900

    wip: working on

--- src/client/theme-default/components/VPPageTeam.vue
+++ src/client/theme-default/components/VPPageTeam.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import VPIconHeart from './icons/VPIconHeart.vue'
 import VPSocialLinks from './VPSocialLinks.vue'
 </script>
 
@@ -12,7 +13,7 @@ import VPSocialLinks from './VPSocialLinks.vue'
           of whom have chosen to be featured below.
         </p>
       </div>
-
+      
       <div class="list">
         <div class="item">
           <article class="box">
@@ -39,6 +40,13 @@ import VPSocialLinks from './VPSocialLinks.vue'
                   { icon: 'twitter', link: 'https://exampl.ecom' }
                 ]"
               />
+            </div>
+
+            <div class="sponsor-box">
+              <button class="sponsor">
+                <VPIconHeart class="sponsor-icon" />
+                Sponsor
+              </button>
             </div>
           </article>
         </div>
@@ -69,6 +77,13 @@ import VPSocialLinks from './VPSocialLinks.vue'
                 ]"
               />
             </div>
+
+            <div class="sponsor-box">
+              <button class="sponsor">
+                <VPIconHeart class="sponsor-icon" />
+                Sponsor
+              </button>
+            </div>
           </article>
         </div>
 
@@ -98,6 +113,13 @@ import VPSocialLinks from './VPSocialLinks.vue'
                 ]"
               />
             </div>
+
+            <div class="sponsor-box">
+              <button class="sponsor">
+                <VPIconHeart class="sponsor-icon" />
+                Sponsor
+              </button>
+            </div>
           </article>
         </div>
       </div>
@@ -107,7 +129,13 @@ import VPSocialLinks from './VPSocialLinks.vue'
 
 <style scoped>
 .VPPageTeam {
+  flex-grow: 1;
   padding: 64px 24px 128px;
+  min-height: calc(100vh - var(--vp-nav-height) - 113px);
+}
+
+.header {
+  text-align: center;
 }
 
 .container {
@@ -116,14 +144,18 @@ import VPSocialLinks from './VPSocialLinks.vue'
 }
 
 .title {
-  line-height: 48px;
-  font-size: 40px;
+  letter-spacing: -0.5px;
+  line-height: 56px;
+  font-size: 48px;
   font-weight: 600;
 }
 
 .lead {
-  max-width: 512px;
-  padding-top: 8px;
+  margin: 0 auto;
+  max-width: 640px;
+  padding-top: 12px;
+  line-height: 28px;
+  font-size: 20px;
   font-weight: 500;
   color: var(--vp-c-text-2);
 }
@@ -132,7 +164,7 @@ import VPSocialLinks from './VPSocialLinks.vue'
   display: flex;
   flex-wrap: wrap;
   gap: 24px;
-  padding-top: 40px;
+  padding-top: 48px;
 }
 
 .item {
@@ -140,21 +172,50 @@ import VPSocialLinks from './VPSocialLinks.vue'
 }
 
 .box {
+  position: relative;
   display: flex;
   flex-direction: column;
   border-radius: 12px;
-  padding: 40px 40px 24px;
+  padding: 48px 48px 0px;
   height: 100%;
   background-color: var(--vp-c-bg-soft);
 }
 
+.sponsor-box {
+  margin: 32px -48px 0;
+  border-top: 2px solid var(--vp-c-bg);
+  padding: 16px 0;
+  text-align: center;
+}
+
+.sponsor {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* border: 1px solid var(--vp-c-sponsor); */
+  padding: 3px 8px;
+  width: 100%;
+  line-height: 20px;
+  font-size: 14px;
+  font-weight: 500;
+  color: var(--vp-c-sponsor);
+}
+
+.sponsor-icon {
+  margin-right: 8px;
+  width: 14px;
+  height: 14px;
+  fill: currentColor;
+}
+
 .avatar {
   position: relative;
-  width: 80px;
-  height: 80px;
   border-radius: 50%;
+  margin: 0 auto;
+  width: 96px;
+  height: 96px;
   overflow: hidden;
-  transform: translate(-4px, -4px);
+  box-shadow: var(--vp-shadow-3);
 }
 
 .img {
@@ -164,12 +225,15 @@ import VPSocialLinks from './VPSocialLinks.vue'
 }
 
 .name {
-  padding-top: 20px;
+  padding-top: 24px;
+  text-align: center;
   font-size: 16px;
   font-weight: 600;
 }
 
 .job {
+  padding-top: 2px;
+  text-align: center;
   line-height: 20px;
   font-size: 14px;
   font-weight: 500;
@@ -177,18 +241,20 @@ import VPSocialLinks from './VPSocialLinks.vue'
 }
 
 .desc {
+  /* display: none; */
   flex-grow: 1;
-  padding-top: 16px;
+  padding-top: 20px;
+  text-align: center;
   line-height: 20px;
   font-size: 14px;
 }
 
 .links {
-  padding-top: 28px;
-  transform: translateX(-8px);
+  padding-top: 24px;
+  /* transform: translateX(-8px); */
 }
 
 .links :deep(.VPSocialLinks) {
-  justify-content: flex-start;
+  /* justify-content: flex-start; */
 }
 </style>
